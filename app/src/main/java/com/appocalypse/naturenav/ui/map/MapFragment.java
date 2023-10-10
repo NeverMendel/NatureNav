1bbeeabfda174315171db87b317c13f95eae1f99
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2023-10-10 23:21:55 +0200

    Add OpenStreetMap

--- app/src/main/java/com/appocalypse/naturenav/ui/map/MapFragment.java
+++ app/src/main/java/com/appocalypse/naturenav/ui/map/MapFragment.java
@@ -1,5 +1,7 @@
 package com.appocalypse.naturenav.ui.map;
 
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +9,25 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.os.BuildCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.appocalypse.naturenav.R;
 import com.appocalypse.naturenav.databinding.FragmentMapBinding;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.library.BuildConfig;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.io.File;
+import java.util.Objects;
 
 public class MapFragment extends Fragment {
 
@@ -18,6 +35,8 @@ public class MapFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        Configuration.getInstance().setUserAgentValue(getString(R.string.app_name));
+
         MapViewModel mapViewModel =
                 new ViewModelProvider(this).get(MapViewModel.class);
 
@@ -25,8 +44,33 @@ public class MapFragment extends Fragment {
                 .inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMap;
-        mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        MapView mapView = binding.osmmap;
+        IMapController controller = mapView.getController();
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setMultiTouchControls(true);
+        mapView.getLocalVisibleRect(new Rect());
+        mapView.setMinZoomLevel(2.0);
+        mapView.setMaxZoomLevel(20.0);
+        controller.setZoom(10.0);
+        controller.setCenter(new GeoPoint(0, 0));
+
+        MyLocationNewOverlay mMyLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(requireContext()), mapView);
+
+        mapView.setExpectedCenter(mMyLocationOverlay.getMyLocation());
+
+        mMyLocationOverlay.enableMyLocation();
+        mMyLocationOverlay.enableFollowLocation();
+        mMyLocationOverlay.setDrawAccuracyEnabled(true);
+        mMyLocationOverlay.runOnFirstFix(() -> {
+            requireActivity().runOnUiThread(() -> {
+                controller.setCenter(mMyLocationOverlay.getMyLocation());
+                controller.animateTo(mMyLocationOverlay.getMyLocation());
+            });
+        });
+
+        mapView.getOverlays().add(mMyLocationOverlay);
+
         return root;
     }
 

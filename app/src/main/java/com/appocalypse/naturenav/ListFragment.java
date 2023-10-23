7627f60d29d5c85242a9ff6b6a2ab640208b09fe
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2023-10-23 23:42:10 +0200

    Add list view with search results

---
+++ app/src/main/java/com/appocalypse/naturenav/ListFragment.java
@@ -0,0 +1,35 @@
+package com.appocalypse.naturenav;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.appocalypse.naturenav.databinding.FragmentListBinding;
+
+import java.util.Objects;
+
+public class ListFragment extends Fragment {
+
+    private FragmentListBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ListViewModel listViewModel =
+                new ViewModelProvider(this).get(ListViewModel.class);
+
+        binding = FragmentListBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(requireContext(), R.layout.list_item, R.id.list_item_title, listViewModel.getData());
+
+        binding.listView.setAdapter(adapter);
+
+        return root;
+    }
+}
--- app/src/main/java/com/appocalypse/naturenav/MainActivity.java
+++ app/src/main/java/com/appocalypse/naturenav/MainActivity.java
@@ -1,14 +1,22 @@
 package com.appocalypse.naturenav;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import org.osmdroid.config.Configuration;
 
 import com.appocalypse.naturenav.MapFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +25,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .setReorderingAllowed(true)
-//                    .add(R.id.main_activity_fragment, MapFragment.class, null)
-//                    .commit();
-//        }
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.main_activity_fragment, MapFragment.class, null)
+                    .commit();
+        }
+
+        findViewById(R.id.search_text_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_activity_fragment, ListFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("Map")
+                        .commit();
+            }
+        });
+
+        findViewById(R.id.action_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().popBackStackImmediate();
+            }
+        });
     }
 }

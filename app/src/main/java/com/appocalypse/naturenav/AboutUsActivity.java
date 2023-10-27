e62fafdbafd05df070a5a789119e3b88dbbcbe5d
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2023-10-27 19:11:07 +0200

    Add AboutUsActivity

---
+++ app/src/main/java/com/appocalypse/naturenav/AboutUsActivity.java
@@ -0,0 +1,25 @@
+package com.appocalypse.naturenav;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        ActionBar actionBar = Objects.requireNonNull(getSupportActionBar());
+
+//      actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//      actionBar.setDisplayShowCustomEnabled(true);
+//      actionBar.setCustomView(R.layout.custom_action_bar);
+
+        actionBar.setTitle(getString(R.string.about_us));
+    }
+}
--- app/src/main/java/com/appocalypse/naturenav/MenuDialogFragment.java
+++ app/src/main/java/com/appocalypse/naturenav/MenuDialogFragment.java
@@ -1,6 +1,7 @@
 package com.appocalypse.naturenav;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,14 +15,20 @@ public class MenuDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.menu_dialog, container, false);
+        View view = inflater.inflate(R.layout.menu_dialog, container, false);
+        view.findViewById(R.id.about_us_menu_button).setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), AboutUsActivity.class);
+            startActivity(intent);
+            dismiss();
+        });
+        return view;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setStyle(DialogFragment.STYLE_NO_FRAME, R.style.Theme_NatureNav);
+//        setStyle(DialogFragment.STYLE_NO_FRAME, R.style.Theme_NatureNav);
         return dialog;
     }
 

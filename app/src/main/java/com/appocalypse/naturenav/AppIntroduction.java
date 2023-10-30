7d939ac7b3a70fd9daaf635f75a5395a3a6f127d
domenicososta <nicososta28@gmail.com>
2023-10-30 23:51:51 +0100

    This commit enables the app to display introduction slides in Italian or English based on the device's language settings, providing a better user experience for international users.

--- app/src/main/java/com/appocalypse/naturenav/AppIntroduction.java
+++ app/src/main/java/com/appocalypse/naturenav/AppIntroduction.java
@@ -20,42 +20,42 @@ public class AppIntroduction extends AppIntro {
 
 
         addSlide(AppIntroFragment.createInstance(
-                "Your Adventure Starts Here",
-                "Explore the beauty of the great outdoors with NatureNav. Discover parks, trails, and more.",
+                getString(R.string.slide1_title),
+                getString(R.string.slide1_description),
                 R.drawable.icon,
                 com.github.appintro.R.color.appintro_background_color
         ));
 
         addSlide(AppIntroFragment.createInstance(
-                "Effortless Search",
-                "NatureNav makes finding points of interest easy with a powerful search feature. Just enter your query, and we'll do the rest.",
+                getString(R.string.slide2_title),
+                getString(R.string.slide2_description),
                 R.drawable.baseline_query_stats_24,
                 com.github.appintro.R.color.appintro_background_color
         ));
 
         addSlide(AppIntroFragment.createInstance(
-                "Explore Points of Interest",
-                "View a wide range of points of interest on our app, from parks to outdoor gyms. You'll never run out of options.",
+                getString(R.string.slide3_title),
+                getString(R.string.slide3_description),
                 R.drawable.baseline_travel_explore_24,
                 com.github.appintro.R.color.appintro_background_color
         ));
 
         addSlide(AppIntroFragment.createInstance(
-                "Rank and Rate",
-                "Give feedback and rate the places you visit. Help build a community of adventurers and enhance their experiences.",
+                getString(R.string.slide4_title),
+                getString(R.string.slide4_description),
                 R.drawable.baseline_stars_24,
                 com.github.appintro.R.color.appintro_background_color
         ));
 
         addSlide(AppIntroFragment.createInstance(
-                "Location permission",
-                "To unlock the full potential of NatureNav, grant us access to your location. Discover nearby nature spots effortlessly.",
+                getString(R.string.slide5_title),
+                getString(R.string.slide5_description),
                 R.drawable.baseline_my_location_24,
                 com.github.appintro.R.color.appintro_background_color
         ));
         addSlide(AppIntroFragment.createInstance(
-                "Get Ready!",
-                "Are you ready to start your adventure with NatureNav? Discover, explore, rate, and connect with fellow nature enthusiasts today!",
+                getString(R.string.slide6_title),
+                getString(R.string.slide6_description),
                 R.drawable.baseline_ads_click_24,
                 com.github.appintro.R.color.appintro_background_color
         ));

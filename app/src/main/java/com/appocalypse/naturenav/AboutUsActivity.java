9d18acc24b72f7ddf29401ae4278c4b3dbf2063c
bonorariccardo892867 <100940685+bonorariccardo892867@users.noreply.github.com>
2023-11-19 23:03:12 +0100

    Second Update

--- app/src/main/java/com/appocalypse/naturenav/AboutUsActivity.java
+++ app/src/main/java/com/appocalypse/naturenav/AboutUsActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import java.util.Objects;
 

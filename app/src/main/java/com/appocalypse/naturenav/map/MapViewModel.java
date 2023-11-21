bbb22b99ed4aee992840aed83c0cc69cc4a36898
domenicososta <nicososta28@gmail.com>
2023-11-21 14:29:50 +0100

    No code changes in MapViewModel
    The class still contains the methods to set and get the location.

--- app/src/main/java/com/appocalypse/naturenav/map/MapViewModel.java
+++ app/src/main/java/com/appocalypse/naturenav/map/MapViewModel.java
@@ -1,10 +1,24 @@
 package com.appocalypse.naturenav.map;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import org.osmdroid.util.GeoPoint;
 
 public class MapViewModel extends ViewModel {
 
+    GeoPoint location = new GeoPoint(0.0, 0.0);
+
     public MapViewModel() {
 
+    }
+
+    public void setLocation(GeoPoint location){
+        this.location = location;
+    }
+
+    public GeoPoint getLocation(){
+        return location;
     }
 }

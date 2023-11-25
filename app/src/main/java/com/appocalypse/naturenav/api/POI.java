10c0a9b7dcd3ce4ddaece1e23d5e7824434a78fa
domenicososta <nicososta28@gmail.com>
2023-11-25 10:32:37 +0100

    Added getAddress function to retrieve the address from geographical coordinates

--- app/src/main/java/com/appocalypse/naturenav/api/POI.java
+++ app/src/main/java/com/appocalypse/naturenav/api/POI.java
@@ -1,9 +1,16 @@
 package com.appocalypse.naturenav.api;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.util.Log;
 
 import org.osmdroid.util.GeoPoint;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class POI {
@@ -12,7 +19,6 @@ public class POI {
     public double lon; // Modificato il tipo da long a double per corrispondere ai dati JSON
     public String type = "Null";
     public Bitmap mThumbnail;
-
     public GeoPoint location;
     public Map<String, String> tags;
 
@@ -32,6 +38,33 @@ public class POI {
 
     public void setTags(Map<String, String> tags) {
         this.tags = tags;
+    }
+    public String getAddress(Context context) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lon, 1);
+
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+
+                // Costruisci l'indirizzo utilizzando gli elementi disponibili (nome della via, città, paese, ecc.)
+                StringBuilder addressBuilder = new StringBuilder();
+
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                    addressBuilder.append(address.getAddressLine(i));
+                    if (i < address.getMaxAddressLineIndex()) {
+                        addressBuilder.append(", ");
+                    }
+                }
+
+                return addressBuilder.toString();
+            }
+        } catch (IOException e) {
+            Log.e("POI", "Errore durante la reverse geocoding", e);
+        }
+
+        return "Indirizzo non disponibile";
     }
     @Override
     public String toString() {

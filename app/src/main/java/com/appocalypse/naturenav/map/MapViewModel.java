c0b9610f21310cd923efeb60d9f85874f1ab0de9
domenicososta <nicososta28@gmail.com>
2023-12-06 23:38:07 +0100

    Update MapViewModel to use MutableLiveData<List<POI>> and provide functions for setting and getting POI list

--- app/src/main/java/com/appocalypse/naturenav/map/MapViewModel.java
+++ app/src/main/java/com/appocalypse/naturenav/map/MapViewModel.java
@@ -1,10 +1,25 @@
 package com.appocalypse.naturenav.map;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.appocalypse.naturenav.api.POI;
+
+import java.util.List;
 
 public class MapViewModel extends ViewModel {
 
+    private MutableLiveData<List<POI>> poiLiveData = new MutableLiveData<>();
+
     public MapViewModel() {
 
+    }
+
+    public MutableLiveData<List<POI>> getPOILiveData() {
+        return poiLiveData;
+    }
+
+    public void setPoiLiveData(List<POI> poi) {
+        poiLiveData.setValue(poi);
     }
 }

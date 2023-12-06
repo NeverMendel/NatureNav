be785571d8cb1e98e71c0042e91367bda8fa7d53
domenicososta <nicososta28@gmail.com>
2023-12-06 23:35:49 +0100

    Added logic to display POIs on the map in MapFragment

--- app/src/main/java/com/appocalypse/naturenav/map/MapFragment.java
+++ app/src/main/java/com/appocalypse/naturenav/map/MapFragment.java
@@ -1,5 +1,7 @@
 package com.appocalypse.naturenav.map;
 
+import com.appocalypse.naturenav.api.POI;
+
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,11 +18,17 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.IMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import com.appocalypse.naturenav.databinding.FragmentMapBinding;
+import com.appocalypse.naturenav.utility.POITypes;
+import com.appocalypse.naturenav.utility.PoiFinder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends Fragment {
     private static final String TAG = "MapFragment";
@@ -28,6 +36,7 @@ public class MapFragment extends Fragment {
     private FragmentMapBinding binding;
     private GeoPoint myLocation;
 
+    private  List<Marker> poiMarkers = new ArrayList<>();
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MapViewModel mapViewModel =
@@ -50,6 +59,30 @@ public class MapFragment extends Fragment {
         controller.setCenter(new GeoPoint(45.440845, 12.315515)); // Venice
 
         MyLocationNewOverlay mMyLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(requireContext()), mapView);
+
+        PoiFinder poiFinder = PoiFinder.getInstance();
+
+        poiFinder.getPoisLiveData().observe(getViewLifecycleOwner(), nuoviPuntiDiInteresse -> {
+            // Rimuovi i marker esistenti
+            for (Marker marker : poiMarkers) {
+                mapView.getOverlays().remove(marker);
+            }
+            poiMarkers.clear(); // Pulisci la lista
+
+            // Aggiungi i nuovi punti di interesse
+            for (POI poi : nuoviPuntiDiInteresse) {
+                Marker marker = new Marker(mapView);
+                marker.setPosition(poi.getGeopoint());
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                marker.setTitle(poi.type); // Personalizza il titolo del marker con il tipo di POI o altre informazioni
+                mapView.getOverlays().add(marker);
+                poiMarkers.add(marker);
+            }
+
+            // Aggiorna la mappa
+            mapView.invalidate();
+        });
+
 
         if (savedInstanceState != null) {
             myLocation = (GeoPoint) savedInstanceState.getSerializable("location");

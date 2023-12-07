f62af1e395cfa338147640d996d711825894b919
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2023-12-07 22:41:22 +0100

    Revert MapViewModel changes

--- app/src/main/java/com/appocalypse/naturenav/map/MapViewModel.java
+++ app/src/main/java/com/appocalypse/naturenav/map/MapViewModel.java
@@ -1,17 +1,10 @@
 package com.appocalypse.naturenav.map;
 
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.appocalypse.naturenav.api.POI;
-
-import java.util.List;
-
 public class MapViewModel extends ViewModel {
-
 
     public MapViewModel() {
 
     }
-
 }

3c2a3f2d5eded7ba57d977cd80ce35cc4c65db8b
Domenico Sosta <94797044+Nico281102@users.noreply.github.com>
2023-12-07 22:47:27 +0100

    Add POI markers in Map (#9)
    * Added logic to display POIs on the map in MapFragment
    
    * Update MapViewModel to use MutableLiveData<List<POI>> and provide functions for setting and getting POI list
    
    * translation of comments into English
    
    * unused members removed
    
    * Marker icon changed
    
    * Fix POI getGeoPoint function name
    
    * Animate to marker and display POI info on marker touch
    
    * Revert MapViewModel changes
    
    ---------
    
    Co-authored-by: Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>

--- app/src/main/java/com/appocalypse/naturenav/map/MapFragment.java
+++ app/src/main/java/com/appocalypse/naturenav/map/MapFragment.java
@@ -1,4 +1,8 @@
 package com.appocalypse.naturenav.map;
+import androidx.core.content.ContextCompat;
+
+import com.appocalypse.naturenav.R;
+import com.appocalypse.naturenav.api.POI;
 
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -16,11 +20,19 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.IMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import com.appocalypse.naturenav.bottomsheet.BottomSheetDialogViewModel;
 import com.appocalypse.naturenav.databinding.FragmentMapBinding;
+import com.appocalypse.naturenav.poiinfo.PoiInfoViewModel;
+import com.appocalypse.naturenav.utility.POITypes;
+import com.appocalypse.naturenav.utility.PoiFinder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends Fragment {
     private static final String TAG = "MapFragment";
@@ -28,6 +40,7 @@ public class MapFragment extends Fragment {
     private FragmentMapBinding binding;
     private GeoPoint myLocation;
 
+    private  List<Marker> poiMarkers = new ArrayList<>();
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MapViewModel mapViewModel =
@@ -36,6 +49,9 @@ public class MapFragment extends Fragment {
         binding = FragmentMapBinding
                 .inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        BottomSheetDialogViewModel bottomSheetDialogViewModel = new ViewModelProvider(requireActivity()).get(BottomSheetDialogViewModel.class);
+        PoiInfoViewModel poiInfoViewModel = new ViewModelProvider(requireActivity()).get(PoiInfoViewModel.class);
 
         MapView mapView = binding.osmmap;
         IMapController controller = mapView.getController();
@@ -50,6 +66,37 @@ public class MapFragment extends Fragment {
         controller.setCenter(new GeoPoint(45.440845, 12.315515)); // Venice
 
         MyLocationNewOverlay mMyLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(requireContext()), mapView);
+
+        PoiFinder poiFinder = PoiFinder.getInstance();
+
+        poiFinder.getPoisLiveData().observe(getViewLifecycleOwner(), newPois -> {
+            // Remove existing markers
+            for (Marker marker : poiMarkers) {
+                mapView.getOverlays().remove(marker);
+            }
+            poiMarkers.clear(); // Clear the list of markers
+
+            // Add new poi markers
+            for (POI poi : newPois) {
+                Marker marker = new Marker(mapView);
+                marker.setPosition(poi.getGeoPoint());
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                marker.setIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_poi_location_24));
+                marker.setOnMarkerClickListener((marker1, mapView1) -> {
+                    controller.animateTo(marker1.getPosition());
+                    bottomSheetDialogViewModel.setDisplayingList(false);
+                    poiInfoViewModel.setDisplayedPoi(poi);
+                    return true;
+                });
+
+                mapView.getOverlays().add(marker);
+                poiMarkers.add(marker);
+            }
+
+            // Refresh the map
+            mapView.invalidate();
+        });
+
 
         if (savedInstanceState != null) {
             myLocation = (GeoPoint) savedInstanceState.getSerializable("location");

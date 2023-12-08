9edd3b92505d86ffdbd93e36becb493a6963b9f2
domenicososta <nicososta28@gmail.com>
2023-12-08 13:15:41 +0100

    - Update PoiInfoFragment based on layout changes

--- app/src/main/java/com/appocalypse/naturenav/poiinfo/PoiInfoFragment.java
+++ app/src/main/java/com/appocalypse/naturenav/poiinfo/PoiInfoFragment.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.appocalypse.naturenav.R;
 import com.appocalypse.naturenav.databinding.FragmentPoiInfoBinding;
+import com.appocalypse.naturenav.utility.POITypes;
+
+import org.osmdroid.bonuspack.location.POI;
 
 public class PoiInfoFragment extends Fragment {
 
@@ -23,6 +28,21 @@ public class PoiInfoFragment extends Fragment {
         View root = binding.getRoot();
 
         poiInfoViewModel = new ViewModelProvider(requireActivity()).get(PoiInfoViewModel.class);
+
+        poiInfoViewModel.getDisplayedPoiLiveData().observe(getViewLifecycleOwner(), poi -> {
+            if (poi != null) {
+                binding.poiTitleTextView.setText(poi.tags.get("name") == null ? requireContext().getString(POITypes.amenityToStringId.get(poi.tags.get("amenity"))) : poi.tags.get("name"));
+                binding.poiAddressTextView.setText(getText( R.string.address ) +" "+ poi.address);
+                binding.poiAirDistanceTextView.setText(getString(R.string.air_distance_placeholder, (int) poi.airDistanceMeters ));
+                binding.poiRoadDistanceTextView.setText(getString(R.string.road_distance_placeholder,  poi.roadDistanceMeters));
+                // convert seconds to minutes
+                binding.poiDurationTextView.setText(getString(R.string.duration_placeholder, poi.roadDistanceSeconds/60));
+
+
+
+            }
+        });
+
 
         return root;
     }

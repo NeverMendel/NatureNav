157bce77ab31c2fb44d6f4f268b815b124b15403
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2024-01-03 22:06:57 +0100

    Add amenities

--- app/src/main/java/com/appocalypse/naturenav/utility/POITypes.java
+++ app/src/main/java/com/appocalypse/naturenav/utility/POITypes.java
@@ -13,18 +13,26 @@ public class POITypes {
     // TODO: Add all amenities that we're interested in
     // https://wiki.openstreetmap.org/wiki/Key:amenity
     public final static Map<String, Integer> amenityToStringId = new HashMap<>(
-            Map.of(
-                    "drinking_water", R.string.drinking_water_amenity,
-                    "restaurant", R.string.restaurant_amenity,
-                    "bicycle_parking", R.string.bicycle_parking_amenity,
-                    "hospital", R.string.hospital_amenity,
-                    "car_wash", R.string.car_wash_amenity,
-                    "bicycle_repair_station", R.string.bicycle_repair_station_amenity,
-                    "bar", R.string.bar_amenity
+            Map.ofEntries(
+                    Map.entry("drinking_water", R.string.drinking_water_amenity),
+                    Map.entry("restaurant", R.string.restaurant_amenity),
+                    Map.entry("bicycle_parking", R.string.bicycle_parking_amenity),
+                    Map.entry("hospital", R.string.hospital_amenity),
+                    Map.entry("car_wash", R.string.car_wash_amenity),
+                    Map.entry("bicycle_repair_station", R.string.bicycle_repair_station_amenity),
+                    Map.entry("bar", R.string.bar_amenity),
+                    Map.entry("library", R.string.library_amenity),
+                    Map.entry("school", R.string.school_amenity),
+                    Map.entry("university", R.string.university_string),
+                    Map.entry("bus_station", R.string.bus_station_amenity),
+                    Map.entry("fuel", R.string.gas_station_amenity),
+                    Map.entry("bank", R.string.bank_amenity),
+                    Map.entry("cinema", R.string.cinema_amenity),
+                    Map.entry("police", R.string.police_station_amenity),
+                    Map.entry("toilets", R.string.toilet_amenity),
+                    Map.entry("shower", R.string.shower_amenity),
+                    Map.entry("parking", R.string.parking_amenity)
             ));
-    public final static Integer[] amenityStringIds = {R.string.drinking_water_amenity, R.string.restaurant_amenity,
-            R.string.bicycle_parking_amenity, R.string.hospital_amenity, R.string.car_wash_amenity,
-            R.string.bicycle_repair_station_amenity, R.string.bar_amenity};
 
     public static List<String> getAmenityStrings(Context context) {
         Integer[] stringIds = new Integer[amenityToStringId.size()];
